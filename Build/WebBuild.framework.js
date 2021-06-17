b9bc172454f495029ee7eea15d77008efea9e188
Bram van Oostrum <brammelam@hotmail.no>
2021-06-17 21:09:31 +0200

    Ghost car! Sound effects!
    Added a ghost replay of each track-record set by the player. Also added a few new sound effects.

--- Build/WebBuild.framework.js
+++ Build/WebBuild.framework.js
@@ -1300,7 +1300,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2784032;
+STATICTOP = STATIC_BASE + 2785456;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3350,7 +3350,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2784032;
+var STATIC_BUMP = 2785456;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
