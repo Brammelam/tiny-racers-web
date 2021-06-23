ba81ffd20a15b8b67b9fde406c61552dfa184762
Bram van Oostrum <brammelam@hotmail.no>
2021-06-23 14:28:23 +0200

    dd

--- Build/WebBuild2.framework.js
+++ Build/WebBuild2.framework.js
@@ -1300,7 +1300,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2829808;
+STATICTOP = STATIC_BASE + 2829872;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3350,7 +3350,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2829808;
+var STATIC_BUMP = 2829872;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
