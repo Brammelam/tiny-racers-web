66d79ac7d2bb3bd812089d14bd2598360686ae20
Bram van Oostrum <brammelam@hotmail.no>
2023-07-01 22:03:05 +0200

    maybe?

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,33 +1,6 @@
-const cacheName = "Brammelam Inc-TinyRacer-1.0";
-const contentToCache = [
-    "Build/WebBuild2.loader.js",
-    "Build/WebBuild2.framework.js",
-    "Build/WebBuild2.data",
-    "Build/WebBuild2.wasm",
-    "TemplateData/style.css"
-
-];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
-    e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
-
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })());
-});
